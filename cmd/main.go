76307b3b608c2e94dca6b7b289fbf8c6b4c51a50
MungaiVic <victornmungai@gmail.com>
2023-07-21 20:38:43 +0300

    Fiber initialization

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Welcome to products")
+	envvar := godotenv.Load(".env")
+	if envvar != nil {
+		log.Fatal("APP_PORT is not set")
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Market 👋!")
+	})
+
+	app.Use(logger.New(logger.Config{
+		Format:     "${cyan}[${time}] ${status} - ${method} ${path}\n",
+		TimeZone:   "Africa/Nairobi",
+		TimeFormat: "2006-01-02 15:04:05",
+	}))
+
+	log.Fatal(app.Listen(fmt.Sprintf(":%v", os.Getenv("APP_PORT"))))
 }
